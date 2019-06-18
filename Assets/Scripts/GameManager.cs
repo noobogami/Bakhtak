99872c08b853be25c84be2dc71a5ecd40e8ec29d
Noobogami <eagle110.ali@gmail.com>
2019-06-18 13:49:26 +0430

    File Added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Table table;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        table = new Table();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
